c7ac4a657fa4069296b29440062d44d0d61129a7
plamenpik <plamen_pik@abv.bg>
2023-02-11 14:47:35 +0200

    Initial init v1.0

---
+++ remove-elemets.js
@@ -0,0 +1,23 @@
+function removeElements() {
+	var cookieBanner = document.querySelector('.kBprba > div:nth-child(1)');
+	var footerBanner = document.getElementById('twilight-sticky-footer-root');
+	var chatButton = document.querySelector('.ScCoreButton-sc-ocjdkq-0.dulmVz.ScButtonIcon-sc-9yap0r-0.HduzH');
+	var primeOffers = document.querySelector('.bZVrjx.prime-offers');
+	if (chatButton) {
+		chatButton.click();
+		console.log('chatButton clicked');
+	}
+	if (cookieBanner) {
+		cookieBanner.remove();
+		console.log('cookieBanner removed');
+	}
+	if (footerBanner) {
+		footerBanner.remove();
+		console.log('footerBanner removed');
+	}
+	if (primeOffers) {
+		primeOffers.remove();
+		console.log('primeOffers removed');
+	}
+}
+setInterval(removeElements, 500);
