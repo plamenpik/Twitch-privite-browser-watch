a1a3f41f471f4ceb54dccaf22fa6075e58a8abd3
plamenpik <plamen_pik@abv.bg>
2023-02-14 17:43:18 +0200

    Version 1.1

--- remove-elemets.js
+++ remove-elemets.js
@@ -1,11 +1,16 @@
-function removeElements() {
+function removeElements() {	
 	var cookieBanner = document.querySelector('.kBprba > div:nth-child(1)');
 	var footerBanner = document.getElementById('twilight-sticky-footer-root');
 	var chatButton = document.querySelector('.ScCoreButton-sc-ocjdkq-0.dulmVz.ScButtonIcon-sc-9yap0r-0.HduzH');
+	var startWatchBtn = document.querySelector('.gRbxbd > button:nth-child(1)');
 	var primeOffers = document.querySelector('.bZVrjx.prime-offers');
 	if (chatButton) {
 		chatButton.click();
 		console.log('chatButton clicked');
+	}
+	if (startWatchBtn) {
+		startWatchBtn.click();
+		console.log('startWatchBtn clicked');
 	}
 	if (cookieBanner) {
 		cookieBanner.remove();
